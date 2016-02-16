25df636ceaccace231eda6b0820029426215e191
fernandofmendes <fernando.mendes@webca.com.br>
2016-02-16 12:32:41 -0200

    finalizar compra

--- public/catalog/language/portuguese-br/payment/pagseguro.php
+++ public/catalog/language/portuguese-br/payment/pagseguro.php
@@ -2,6 +2,6 @@
 $_['text_title'] 		= 'PagSeguro - Cartão de Débito, Crédito ou Boleto';
 $_['text_information'] 	= 'Ao clicar no botão abaixo, você será redirecionado para o site do PagSeguro para efeturar o pagamento. Ao concluir o pagamento você retornará automaticamente para a loja.';
 $_['text_wait']         = 'Por favor, aguarde!';
-$_['button_confirm_pagseguro'] = 'Confirmar e ir para o PagSeguro';
+$_['button_confirm_pagseguro'] = 'finalizar compra';
 $_['text_extra_amount']	= 'Frete e Taxas';
 ?>
