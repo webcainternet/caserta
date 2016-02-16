e4643b6dd70fe2e1095845eab8f6f1475cd36402
fernandofmendes <fernando.mendes@webca.com.br>
2016-02-16 12:40:39 -0200

    texto

--- public/catalog/language/portuguese-br/checkout/success.php
+++ public/catalog/language/portuguese-br/checkout/success.php
@@ -3,7 +3,7 @@
 $_['heading_title'] = 'Seu pedido foi realizado com sucesso!';
 
 // Text
-$_['text_customer'] = '<br><p>Você pode ver o histórico dos seus pedidos acessando <a href="%s">sua conta</a> e clicando em <a href="%s">Histórico de Pedidos</a>.</p><p>Caso tenha alguma dúvida, por favor, <a href="%s">clique aqui</a>.</p><p>Obrigado por comprar na Caserta!</p>';
+$_['text_customer'] = '<br><p>Você pode ver o histórico dos seus pedidos acessando o menu <a href="%s">minha conta</a> e clicando em <a href="%s">Histórico de Pedidos</a>.</p><p>Caso tenha alguma dúvida, por favor, <a href="%s">clique aqui</a>.</p><p>Obrigado por comprar na Caserta!</p>';
 $_['text_guest']    = '<br><p>Caso você tenha dúvidas entre em contato conosco clicando <a href="%s">aqui</a>.</p><p>Obrigado por comprar em nossa loja!</p>';
 $_['text_basket']   = 'Meu Carrinho';
 $_['text_checkout'] = 'Finalizar Compra';
